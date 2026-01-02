ba68afab2b13cb17975be4b91093887fa9b413e1
Rayan H. <rayan.hasnoun34@gmail.com>
2026-01-02 19:27:03 +0100

    Add tests unit for TeacherController & UserController

---
+++ back/src/test/java/com/openclassrooms/starterjwt/controllers/TeacherControllerTest.java
@@ -0,0 +1,75 @@
+package com.openclassrooms.starterjwt.controllers;
+
+import com.openclassrooms.starterjwt.dto.TeacherDto;
+import com.openclassrooms.starterjwt.mapper.TeacherMapper;
+import com.openclassrooms.starterjwt.models.Teacher;
+import com.openclassrooms.starterjwt.services.TeacherService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class TeacherControllerTest {
+
+    @Mock
+    private TeacherService teacherService;
+
+    @Mock
+    private TeacherMapper teacherMapper;
+
+    @InjectMocks
+    private TeacherController teacherController;
+
+    @Test
+    void shouldReturnTeacherWhenIdIsValid() {
+        Teacher teacher = new Teacher();
+        TeacherDto teacherDto = new TeacherDto();
+
+        when(teacherService.findById(1L)).thenReturn(teacher);
+        when(teacherMapper.toDto(teacher)).thenReturn(teacherDto);
+
+        ResponseEntity<?> response = teacherController.findById("1");
+
+        assertEquals(200, response.getStatusCodeValue());
+        assertEquals(teacherDto, response.getBody());
+    }
+
+    @Test
+    void shouldReturn404WhenTeacherNotFound() {
+        when(teacherService.findById(1L)).thenReturn(null);
+
+        ResponseEntity<?> response = teacherController.findById("1");
+
+        assertEquals(404, response.getStatusCodeValue());
+    }
+
+    @Test
+    void shouldReturn400WhenIdIsInvalid() {
+        ResponseEntity<?> response = teacherController.findById("abc");
+
+        assertEquals(400, response.getStatusCodeValue());
+    }
+
+
+    @Test
+    void shouldReturnAllTeachers() {
+        List<Teacher> teachers = List.of(new Teacher(), new Teacher());
+        List<TeacherDto> teacherDtos = List.of(new TeacherDto(), new TeacherDto());
+
+        when(teacherService.findAll()).thenReturn(teachers);
+        when(teacherMapper.toDto(teachers)).thenReturn(teacherDtos);
+
+        ResponseEntity<?> response = teacherController.findAll();
+
+        assertEquals(200, response.getStatusCodeValue());
+        assertEquals(teacherDtos, response.getBody());
+    }
+}
---
+++ back/src/test/java/com/openclassrooms/starterjwt/controllers/UserControllerTest.java
@@ -0,0 +1,135 @@
+package com.openclassrooms.starterjwt.controllers;
+
+import com.openclassrooms.starterjwt.dto.UserDto;
+import com.openclassrooms.starterjwt.mapper.UserMapper;
+import com.openclassrooms.starterjwt.models.User;
+import com.openclassrooms.starterjwt.services.UserService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class UserControllerTest {
+
+    @Mock
+    private UserService userService;
+
+    @Mock
+    private UserMapper userMapper;
+
+    @InjectMocks
+    private UserController userController;
+
+    @AfterEach
+    void clearSecurityContext() {
+        SecurityContextHolder.clearContext();
+    }
+
+    // ---------- findById ----------
+
+    @Test
+    void shouldReturnUserWhenIdIsValid() {
+        User user = new User();
+        UserDto userDto = new UserDto();
+
+        when(userService.findById(1L)).thenReturn(user);
+        when(userMapper.toDto(user)).thenReturn(userDto);
+
+        ResponseEntity<?> response = userController.findById("1");
+
+        assertEquals(200, response.getStatusCodeValue());
+        assertEquals(userDto, response.getBody());
+    }
+
+    @Test
+    void shouldReturn404WhenUserNotFound() {
+        when(userService.findById(1L)).thenReturn(null);
+
+        ResponseEntity<?> response = userController.findById("1");
+
+        assertEquals(404, response.getStatusCodeValue());
+    }
+
+    @Test
+    void shouldReturn400WhenIdIsInvalid() {
+        ResponseEntity<?> response = userController.findById("abc");
+
+        assertEquals(400, response.getStatusCodeValue());
+    }
+
+    @Test
+    void shouldDeleteUserWhenAuthenticatedUserMatches() {
+        User user = new User();
+        user.setEmail("test@test.com");
+
+        when(userService.findById(1L)).thenReturn(user);
+
+        UserDetails userDetails = mock(UserDetails.class);
+        when(userDetails.getUsername()).thenReturn("test@test.com");
+
+        Authentication authentication = mock(Authentication.class);
+        when(authentication.getPrincipal()).thenReturn(userDetails);
+
+        SecurityContext securityContext = mock(SecurityContext.class);
+        when(securityContext.getAuthentication()).thenReturn(authentication);
+
+        SecurityContextHolder.setContext(securityContext);
+
+        ResponseEntity<?> response = userController.save("1");
+
+        assertEquals(200, response.getStatusCodeValue());
+        verify(userService).delete(1L);
+    }
+
+    @Test
+    void shouldReturn401WhenAuthenticatedUserDoesNotMatch() {
+        // GIVEN
+        User user = new User();
+        user.setEmail("other@test.com");
+
+        when(userService.findById(1L)).thenReturn(user);
+
+        UserDetails userDetails = mock(UserDetails.class);
+        when(userDetails.getUsername()).thenReturn("test@test.com");
+
+        Authentication authentication = mock(Authentication.class);
+        when(authentication.getPrincipal()).thenReturn(userDetails);
+
+        SecurityContext securityContext = mock(SecurityContext.class);
+        when(securityContext.getAuthentication()).thenReturn(authentication);
+
+        SecurityContextHolder.setContext(securityContext);
+
+        ResponseEntity<?> response = userController.save("1");
+
+        assertEquals(401, response.getStatusCodeValue());
+        verify(userService, never()).delete(anyLong());
+    }
+
+    @Test
+    void shouldReturn404WhenDeletingUnknownUser() {
+        when(userService.findById(1L)).thenReturn(null);
+
+        ResponseEntity<?> response = userController.save("1");
+
+        assertEquals(404, response.getStatusCodeValue());
+    }
+
+    @Test
+    void shouldReturn400WhenDeleteIdIsInvalid() {
+        ResponseEntity<?> response = userController.save("abc");
+
+        assertEquals(400, response.getStatusCodeValue());
+    }
+}
